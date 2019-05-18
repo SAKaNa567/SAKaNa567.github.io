85281960be1771e5fc33ea706e870f82912b7ef3
naoto <s1240147@u-aizu.ac.jp>
2019-05-18 16:18:40 +0900

    不具合の編集

--- index.js
+++ index.js
@@ -1,19 +1,40 @@
 // Naoto Kito, 5/17 simple stop watch 
 
-var startTime;
-var updatedTime;
+let startTime = 0;
+let updatedTime = 0;
 let savedTime = 0;
-let paused = 0;
+let paused = 1;//止まっている状態.
 let differenceTime = 0;
+//タイマーをリセットしておく。
+let showtime = document.getElementById('time');
+showtime.innerHTML = `${displayTime(0)}`;
+let interval = 0;
 
+//スタートボタンの処理.
 function startButton() {
-    startTime = new Date().getTime();
-    interval = setInterval(getShowTime,1000);
+    if(!startTime) {//startTimeが0のとき.
+        startTime = new Date().getTime();
+        paused = 0;//動いた状態にする.
+        interval = setInterval(getShowTime,1000)
+        document.getElementById('start').innerHTML = "<input type='button' id='start_button' value='reset' onclick='startButton();'>";
+    } else {
+        document.getElementById('start').innerHTML =  "<input type='button' id='start_button' value='start' onclick='startButton();'>";
+        clearInterval(interval);
+        //タイマーの値を初期化する.
+        showtime.innerHTML =    `${displayTime(0)}`;
+
+        //全ての値を初期化する.
+        startTime =  0;
+        updatedTime = 0;
+        savedTime = 0;
+        paused = 1;
+        differenceTime = 0;
+    }
 }
 
+//今の時間を表示する関数
 function getShowTime() {
     updatedTime = new Date().getTime();
-    let showtime = document.getElementById('time');
     differenceTime = updatedTime - startTime;
     if (savedTime){
         // showtime.innerHTML = `time : ${differenceTime + savedTime}` 
@@ -23,20 +44,7 @@ function getShowTime() {
         showtime.innerHTML =   `${displayTime(differenceTime)}`;
     }
 }
-
-function pauseButton() {
-    if (paused){ //止まってたら再開する.
-        // 再開させる＋止まってる状態を無効にする
-        paused = 0
-        savedTime = differenceTime+savedTime;//前回止めた分の＋前回より前の分
-        startButton();
-    } else { //動いてたら止める.
-        // 今までの時間を記録＋止まってる状態を有効にする.
-        clearInterval(interval);
-        paused = 1 // paused===1状態
-    }
-}
-
+//converter関数
 function displayTime(time) {
     let hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     let minutes = Math.floor((time % ( 1000 * 60 * 60)) / (1000 * 60));
@@ -44,3 +52,23 @@ function displayTime(time) {
     let converted_time = ((hours > 0) ? hours+" hour " : "") + ((minutes > 0) ? minutes+" min " : "") + seconds + " sec";
     return converted_time
 }
+
+// pauseボタンが押されたときの処理.
+function pauseButton() {
+    console.log(paused);
+    if (paused){ //止まっているときの処理.（まだ開始されていない場合も含む。）
+        // 開始された状態からの場合は、再開させる＋止まってる状態を無効にする
+        //startしてからのpauseからのpauseであった場合.
+        if(startTime){//再開を行う.
+            paused = 0;
+            startTime = new Date().getTime();
+            interval = setInterval(getShowTime,1000);
+        }
+    } else if(paused === 0) { //動いてたら止める.
+        // 今までの時間を記録＋止まってる状態を有効にする.
+        savedTime = differenceTime+savedTime;//前回止めた分の＋前回より前の分
+        clearInterval(interval)
+        paused = 1 // 止まっている状態
+    }
+}
+
