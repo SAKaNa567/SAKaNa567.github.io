9ae52babeab17486dd74f622413776081fd6a7d7
naoto <s1240147@u-aizu.ac.jp>
2019-05-18 22:47:20 +0900

    try offline version 3

--- service-worker.js
+++ service-worker.js
@@ -3,10 +3,12 @@ const FILES_TO_CACHE = [
     '/offline.html',
 ]
 
-self.addEventListener('install', function(evt){
+
+
+self.addEventListener('install', function(event){
     console.log('[ServiceWorker] Install');
     //prechache static resource here.
-    evt.waitUntil(
+    event.waitUntil(
         // caches.open() open cache.
         caches.open(CACH_NAME).then((cache) => {
             console.log('[ServiceWorker] Pre-caching offline page');
@@ -15,10 +17,10 @@ self.addEventListener('install', function(evt){
     );
 });
 
-self.addEventListener('activate', function(evt){
+self.addEventListener('activate', function(event){
     console.log('[ServiceWorker] Activate');
     //remove previous cached data from disk.
-    evt.waitUntil(
+    event.waitUntil(
         caches.keys().then((keyList) => {
             return Promise.all(keyList.map((key) => {
                 if(key !== CACHE_NAME){
@@ -30,14 +32,14 @@ self.addEventListener('activate', function(evt){
     );
 });
 
-self.addEventListener('fetch', function(evt){
+self.addEventListener('fetch', function(event){
     //add fetch event handler here.
-    if(evt.request.mode !== 'navigate') {
+    if(event.request.mode !== 'navigate') {
         //not a page navigation, bail.
         return;
     }
-    evt.respondWith(
-        fetch(evt.request)
+    event.respondWith(
+        fetch(event.request)
             .cache( () => {
                 return caches.open(CACHE_NAME)
                     .then((cache) => {
