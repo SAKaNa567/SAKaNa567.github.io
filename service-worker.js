2b4008477bbae834249f5d1b30dd32e25f2de293
naoto <s1240147@u-aizu.ac.jp>
2019-05-18 22:21:34 +0900

    try offline version

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,48 @@
+//update cache names any time any of the cached files change.
+const FILES_TO_CACHE = [
+    '/offline.html',
+]
+
 self.addEventListener('install', function(e){
     console.log('[ServiceWorker] Install');
+    //prechache static resource here.
+    evt.waitUntil(
+        // caches.open() open cache.
+        caches.open(CACH_NAME).then((cache) => {
+            console.log('[ServiceWorker] Pre-caching offline page');
+            return cache.addAll(FILES_TO_CACHE);
+        })
+    );
 });
 
 self.addEventListener('activate', function(e){
     console.log('[ServiceWorker] Activate');
+    //remove previous cached data from disk.
+    evtwaitUntil(
+        caches.keys().then((keyList) => {
+            return Promise.all(keyList.map((key) => {
+                if(key !== CACHE_NAME){
+                    console.log('[ServiceWorker] Removing old cache', key);
+                    return caches.delete(key);
+                }
+            }));
+        })
+    );
 });
 
-self.addEventListener('fetch', function(event){});
+self.addEventListener('fetch', function(event){
+    //add fetch event handler here.
+    if(evt.request.mode !== 'navigate') {
+        //not a page navigation, bail.
+        return;
+    }
+    evt.respondWith(
+        fetch(evt.request)
+            .cache( () => {
+                return caches.open(CACHE_NAME)
+                    .then((cache) => {
+                        return cache.match('offline.html');
+                    });
+            })
+    );
+});
